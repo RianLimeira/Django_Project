b6f2a1cbb48dd06cfeb8ff292e0dbdc6352b3c52
RianLimeira <rianlimeira@gmail.com>
2022-08-16 17:48:48 -0300

    contas matematica pela url

--- core/views.py
+++ core/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos </h1>'.format(nome, idade))
+def hello(request, num1, num2):
+    texto = "<h1>Primeiro numero: {} segundo: {} </h1>".format(num1, num2)
+    soma = num1 + num2
+    multiplicacao = num1 * num2
+
+    contas = "<h2>soma: {} </h2>" \
+             "<h2>multiplicacao: {}".format(soma, multiplicacao)
+    tela = (texto + " " + contas)
+    return HttpResponse({tela})
