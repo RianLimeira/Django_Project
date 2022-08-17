354a48d4b6b5638e9bbe2f1e7674f7a2ef649163
RianLimeira <rianlimeira@gmail.com>
2022-08-17 11:43:40 -0300

    templates and list events

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from core.models import Evento
+
 
 # Create your views here.
+
+"""Cria uma função de index para redirecionamento"""
+#def index(request):
+#   return redirect('/agenda')
+
+def lista_eventos(request):
+    """
+    Filtro de usuarios
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    """
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
