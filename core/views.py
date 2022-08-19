c42e9bad6d040072f1e37423ff18967447f76c14
RianLimeira <rianlimeira@gmail.com>
2022-08-19 15:54:48 -0300

    filter URL and Json

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 # Create your views here.
 
@@ -37,17 +40,17 @@ def submit_login(request):
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
-        dateEvento = request.POST.get('data_evento')
+        data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
         id_evento = request.POST.get('id_evento')
         if id_evento:
             """validação de usuario para Editar"""
-            evento = Evento.objects.get('id_evento')
+            evento = Evento.objects.get(id=id_evento)
             if evento.usuario == usuario:
                 evento.titulo = titulo
                 evento.descricao = descricao
-                evento.data_evento = dateEvento
+                evento.data_evento = data_evento
                 evento.save()
             """Edição pura sem validação de usuario"""
             #Evento.objects.filter(id=id_evento).update(titulo=titulo,
@@ -55,7 +58,7 @@ def submit_evento(request):
             #                                           descricao=descricao)
         else:
             Evento.objects.create(titulo=titulo,
-                                  data_evento=dateEvento,
+                                  data_evento=data_evento,
                                   descricao=descricao,
                                   usuario=usuario)
 
@@ -64,7 +67,10 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404
     if usuario == evento.usuario:
         evento.delete()
     return redirect('/')
@@ -78,7 +84,9 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     """
     user = request.user
-    evento = Evento.objects.filter(usuario=user)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=user,
+                                   data_evento__gt=data_atual)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
@@ -89,4 +97,13 @@ def evento(request):
     dados = {}
     if id_evento:
         dados['evento'] = Evento.objects.get(id=id_evento)
+    else:
+        raise Http404()
     return render(request, 'evento.html', dados)
+
+
+def json_lista_evento(request, id_usuario):
+    user = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=user).values('id', 'titulo')
+
+    return JsonResponse(list(evento), safe=False)
